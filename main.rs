6a209b1333badd5ccd99ac9d7a74ee485a03438a
Frederick Nicklas Ambo Eggert Eggertsen <frederick@tacaly.com>
2022-06-10 18:27:44 +0200

    Project init

---
+++ main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Statements here are executed when the compiled binary is called
+    let job_hunt_title = "[JobHunt]";
+    // Print text to the console
+    println!("{job_hunt_title} Starting Jobhunt!");
+
+}
