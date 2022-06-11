c25c21cad293f55e6c094938509016cf592cbe4b
Frederick Nicklas Ambo Eggert Eggertsen <frederick@tacaly.com>
2022-06-11 02:09:29 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -5,3 +5,11 @@ fn main() {
     println!("{job_hunt_title} Starting Jobhunt!");
 
 }
+
+fn findjob() {
+// gets the jobgen lib to find a job, and then report it to the database
+}
+
+fn getjob() {
+// make the request to send the cover letter and CV to the company looking for people.
+}
